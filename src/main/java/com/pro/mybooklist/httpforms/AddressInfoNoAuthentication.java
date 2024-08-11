8d70d66b58a1d6943ca4781951940f7f9bc73cc2
jessie-sr <jessie_sr@163.com>
2024-08-12 00:09:40 +0800

    Updated AddressInfoNoAuthentication

--- src/main/java/com/pro/mybooklist/httpforms/AddressInfoNoAuthentication.java
+++ src/main/java/com/pro/mybooklist/httpforms/AddressInfoNoAuthentication.java
@@ -11,9 +11,10 @@ public class AddressInfoNoAuthentication {
 	private String note;
 	private Long cartid;
 	private String password;
-	
-	public AddressInfoNoAuthentication() {}
-	
+
+	public AddressInfoNoAuthentication() {
+	}
+
 	public AddressInfoNoAuthentication(String firstname, String lastname, String country, String city, String street,
 			String postcode, String email, String note, Long cartid, String password) {
 		this.firstname = firstname;
@@ -31,60 +32,79 @@ public class AddressInfoNoAuthentication {
 	public String getFirstname() {
 		return firstname;
 	}
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+
 	public String getLastname() {
 		return lastname;
 	}
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getStreet() {
 		return street;
 	}
+
 	public void setStreet(String street) {
 		this.street = street;
 	}
+
 	public String getPostcode() {
 		return postcode;
 	}
+
 	public void setPostcode(String postcode) {
 		this.postcode = postcode;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getNote() {
 		return note;
 	}
+
 	public void setNote(String note) {
 		this.note = note;
 	}
+
 	public Long getCartid() {
 		return cartid;
 	}
+
 	public void setcartid(Long cartid) {
 		this.cartid = cartid;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
