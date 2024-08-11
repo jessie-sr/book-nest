f80ce6eeafff3565bc4915ad835378746038786b
jessie-sr <jessie_sr@163.com>
2024-08-11 21:32:21 +0800

    Updated AuthenticationService

--- src/main/java/com/pro/mybooklist/service/AuthenticationService.java
+++ src/main/java/com/pro/mybooklist/service/AuthenticationService.java
@@ -18,7 +18,7 @@ public class AuthenticationService {
 	static final long EXPIRATION_TIME = 864_000_00;
 	static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 	static final String PREFIX = "Bearer";
-	
+
 	public String getToken(String username) {
 		String token = Jwts.builder()
 				.setSubject(username)
@@ -27,10 +27,10 @@ public class AuthenticationService {
 				.compact();
 		return token;
 	}
-	
+
 	public String getAuthUser(HttpServletRequest request) {
 		String token = request.getHeader(HttpHeaders.AUTHORIZATION);
-		
+
 		if (token != null) {
 			String user = Jwts.parserBuilder()
 					.setSigningKey(key)
@@ -38,12 +38,11 @@ public class AuthenticationService {
 					.parseClaimsJws(token.replace(PREFIX, ""))
 					.getBody()
 					.getSubject();
-			
+
 			if (user != null)
 				return user;
 		}
-		
+
 		return null;
 	}
 }
-
